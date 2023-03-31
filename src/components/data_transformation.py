116ee76ffaf4f06442af8fab0f931a6d42fa735d
Dr. V. Swaroopa Rani <swarajesh78@gmail.com>
2023-03-31 12:40:41 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -76,8 +76,8 @@ class DataTransformation:
     def initiate_data_transformation(self,train_path,test_path):
 
         try:
-            train_df=pd.read_csv(train_path,encoding='ISO-8859-1')
-            test_df=pd.read_csv(test_path,encoding='ISO-8859-1')
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
 
             logging.info("Read train and test data completed")
 
